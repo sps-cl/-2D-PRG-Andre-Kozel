bcb940324c74a69aaaaeff50694a7df662707c1c
andrekozel <144631880+andrekozel@users.noreply.github.com>
2023-11-05 19:16:49 +0100

    Update gameobject.js

--- SP-OOP-1/gameobject.js
+++ SP-OOP-1/gameobject.js
@@ -1,21 +1,21 @@
 class GameObject extends Positionable {
-    constructor(position)  {
-        super(position);
-        this.speed = new Vector(0,0);
-        this.minBounds = new Vector(-Infinity, -Infinity);
-        this.maxBounds = new Vector(Infinity, Infinity);
+    constructor(position) { 
+        super (position)
+        this.speed = new Vector (0, 0)
+        this.minBounds = new Vector (-Infinity, -Infinity)
+        this.maxBounds - new Vector (Infinity, Infinity)
     }
 
     move() {
-        this.position.add(this.speed);
+        this.position.add(this.speed)
     }
 
-    draw(context) {
+    draw (context) {
         this.sprite.draw(context)
     }
 
-    collideWith(other) {
+    collideWith (other) {
         return this.collider.collideWith(other.collider)
     }
+
 }
-    
