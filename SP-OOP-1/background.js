7dcc3571e7038eab19fc5da37fb7a175f42f7e72
andrekozel <144631880+andrekozel@users.noreply.github.com>
2023-11-05 19:20:33 +0100

    Update background.js

--- SP-OOP-1/background.js
+++ SP-OOP-1/background.js
@@ -1,12 +1,11 @@
-class Background{
-    constructor (width,height) {
-        this.width = width;
-        this.height = height;
-        this.image = document.getElementById("background");
+class Background {
+    constructor(width, height) {
+        this.width = width
+        this.height = height
+        this.image = document.getElementById("background")
     }
-
+ 
     draw(context) {
-        context.drawImage(this.image, 0,0, this.width, this.height);
+        context.drawImage(this.image, 0, 0, this.width, this.height)
     }
 }
-    
