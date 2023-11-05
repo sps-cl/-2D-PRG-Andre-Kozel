a56e1e70bc9dafc125a6580ce7fc8b6af158c69b
andrekozel <144631880+andrekozel@users.noreply.github.com>
2023-11-05 19:18:35 +0100

    Update cactus.js

--- SP-OOP-1/cactus.js
+++ SP-OOP-1/cactus.js
@@ -1,14 +1,14 @@
 class Cactus extends GameObject {
-    constructor(position, speed, width, height) {
-        super(position);
-        this.speed = speed;
-        this.sprite = new Sprite("cactus", position, width, height);
-        this.collider = new RectCollider(position, width, height);
+    constructor (position,speed, width, height) {
+        super (position)
+        this.speed = speed
+        this.sprite = new Sprite ("cactus", position, width, height)
+        this.collider = new RectCollider (position, width, height)
     }
-
-    move() {
+    move () {
         super.move()
-        if (this.collider.rightEdge < this.minBounds.x) { 
-        } else this.onSceneExit();
+        if (this.collider.rightEdge < this.minBounds.x) {
+            this.onSceneExit()
+        }
     }
 }
